7d0f1c9a0494276fe907040f1a8576a7953ebee7
vivchik1337 <v.podlipnyak@gmail.com>
2018-09-09 18:52:01 +0300

    added realm lib; deleted old interface;

--- app/src/main/java/com/noneadstask/adapter/ListClicks.java
+++
@@ -1,9 +0,0 @@
-package com.noneadstask.adapter;
-
-import com.noneadstask.model.Person;
-
-public interface ListClicks {
-    public void onFavoriteClick(Person person);
-    public void onCommentClick(Person person);
-    public void onSearch(String searchText);
-}
